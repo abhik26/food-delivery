954bbdc13ef66188840c7f2ed947e6ee9b800fd4
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2024-11-07 22:58:26 +0530

    Add Eureka server

---
+++ eureka-server/src/test/java/abhik26/food_delivery/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package abhik26.food_delivery.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
